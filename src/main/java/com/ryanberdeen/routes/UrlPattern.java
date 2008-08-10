adfc64c71b4b44410dfcdc827e07cd5dcc0800a0
Ryan Berdeen <ryan@ryanberdeen.com>
2008-08-10 16:40:26 -0700

    fixed applying parameter values that had different default values

--- src/main/java/com/ryanberdeen/routes/UrlPattern.java
+++ src/main/java/com/ryanberdeen/routes/UrlPattern.java
@@ -442,7 +442,7 @@ public class UrlPattern implements Cloneable {
 		public UrlSegment apply(Map<String, String> parameters, Map<String, String> defaultParameters) {
 			String value = parameters.get(name);
 			if (value != null) {
-				return new StaticSegment(value, !defaultParameters.containsKey(name));
+				return new StaticSegment(value, !value.equals(defaultParameters.get(name)));
 			}
 			else {
 				return clone();
