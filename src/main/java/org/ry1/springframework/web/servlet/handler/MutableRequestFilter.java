84c328f1bc4e20917455d714a4e1a4a032809f56
Ryan Berdeen <ryan@ryanberdeen.com>
2008-12-31 00:30:58 -0800

    moved spring xml support to routes-spring-xml

--- src/main/java/org/ry1/springframework/web/servlet/handler/MutableRequestFilter.java
+++ src/main/java/org/ry1/springframework/web/servlet/handler/MutableRequestFilter.java
@@ -2,21 +2,25 @@ package org.ry1.springframework.web.servlet.handler;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.filter.OncePerRequestFilter;
 
 /** Replaces requests with <code>MutableRequestWrapper</code> to allow for extended parameters.
  * @see org.ry1.springframework.web.util.ExtendedParameters.Strategy.WRAPPER
  * @author rberdeen
  */
-public class MutableRequestFilter extends OncePerRequestFilter {
+public class MutableRequestFilter implements Filter {
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-		chain.doFilter(new MutableRequestWrapper(request), response);
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		chain.doFilter(new MutableRequestWrapper((HttpServletRequest) request), response);
 	}
+
+	public void destroy() {}
 }
